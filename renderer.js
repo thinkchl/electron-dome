40d65dcb5a59a405cb7a8a115eb3f19b6086dfc2
chenhuaili <chenhuaili050@chinasoftinc.com>
2024-01-11 18:06:36 +0800

    firsh commit

---
+++ renderer.js
@@ -0,0 +1,8 @@
+    const replaceText = (selector, text) => {
+      const element = document.getElementById(selector)
+      if (element) element.innerText = text
+    }
+  
+    for (const dependency of ['chrome', 'node', 'electron']) {
+      replaceText(`${dependency}-version`, versions[dependency]())
+    }
